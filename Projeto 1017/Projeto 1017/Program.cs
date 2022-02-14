2249417d6c59140c0b3a35ee2dc46e30db5cad7a
karluze <karluzem@gmail.com>
2022-02-14 16:27:12 -0300

    Projeto 1017

---
+++ Projeto 1017/Projeto 1017/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double H, V, resultado;
+
+            H = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            V = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+
+            resultado = (H * V) / 12;
+
+            Console.WriteLine(resultado.ToString("F3", CultureInfo.InvariantCulture));
+
+
+            //Console.ReadLine();
+        }
+    }
+}
